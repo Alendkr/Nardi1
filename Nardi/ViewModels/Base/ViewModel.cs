c40daaf727407281c0155e1085db8c9a59fcabbd
Aleksey <alendk.real@gmail.com>
2023-05-20 20:29:43 +0300

    Добавьте файлы проекта.

---
+++ Nardi/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Nardi.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field,T value, [CallerMemberName] string PropertyName=null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;          
+        }
+    }
+}
---
+++ Nardi/ViewModels/GameWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Nardi.Infrastructure.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Nardi.ViewModels.Base;
+
+namespace Nardi.ViewModels
+{
+    class GameWindowViewModel
+    {
+    }
+}
